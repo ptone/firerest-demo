27d49e457ac6159ddb0f1a5d4bd6c1e3c21e74ac
Preston <dummy@mail.com>
2019-12-24 02:01:19 +0000

    update deployment info for public and private gateway

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -1,4 +1,5 @@
 
 docker build -t gcr.io/ptone-misc/api-demo .
 docker push gcr.io/ptone-misc/api-demo
-gcloud run deploy --image gcr.io/ptone-misc/api-demo api-demo
+gcloud run deploy -q --async --image gcr.io/ptone-misc/api-demo api-demo --no-allow-unauthenticated
+gcloud run deploy -q --async --image gcr.io/ptone-misc/api-demo api-demo-pulic --allow-unauthenticated
