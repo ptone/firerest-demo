3feac9bf7d12dd56f313ca36f1ca5ef854aca999
Preston <dummy@mail.com>
2019-12-30 21:41:00 +0000

    also deploy a public version of api

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -2,4 +2,4 @@
 docker build -t gcr.io/ptone-misc/api-demo .
 docker push gcr.io/ptone-misc/api-demo
 gcloud run deploy -q --async --image gcr.io/ptone-misc/api-demo api-demo --no-allow-unauthenticated
-gcloud run deploy -q --async --image gcr.io/ptone-misc/api-demo api-demo-pulic --allow-unauthenticated
+gcloud run deploy -q --async --image gcr.io/ptone-misc/api-demo api-demo-public --allow-unauthenticated
