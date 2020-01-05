635ffa814f55c16fb8a13b536d386b1fb7907123
Preston <dummy@mail.com>
2020-01-05 21:56:05 +0000

    fix public api for cors and frontend

--- demo/main.py
+++ demo/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from app.v1.router import api_router
 
@@ -8,7 +8,7 @@ app = FastAPI(title="API Demo", openapi_url="/api/v1/openapi.json")
 # app = FastAPI(title="Test")
 
 # CORS
-origins = []
+origins = ["*"]
 
 # Set all CORS enabled origins
 # if config.BACKEND_CORS_ORIGINS:
@@ -16,13 +16,13 @@ origins = []
 #     for origin in origins_raw:
 #         use_origin = origin.strip()
 #         origins.append(use_origin)
-#     app.add_middleware(
-#         CORSMiddleware,
-#         allow_origins=origins,
-#         allow_credentials=True,
-#         allow_methods=["*"],
-#         allow_headers=["*"],
-#     ),
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # app.include_router(api_router, prefix=config.API_V1_STR)
 app.include_router(api_router, prefix="/api/v1")
